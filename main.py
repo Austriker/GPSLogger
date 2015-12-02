9f6fc980623dca0464c88060482fbfec447b09f5
Hugo Serrat <hu.serrat@gmail.com>
2015-12-02 14:32:42 +0100

    added basic logging

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 from gps3 import gps3
 import time
+import logging
 
-if __name__ == "__main__":
+logger = logging.getLogger(__name__)
 
+handler = logging.FileHandler('hello.log')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+def main():
     connection = gps3.GPSDSocket()
     fix = gps3.Fix()
 
     try:
         for data in connection:
             if data:
-                fix.refresh(data)
+                logger.info(data)
             else:
                 pass
 
@@ -19,3 +28,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         connection.close()
         print("\nTerminated by user\nGood Bye.\n")
+
+
+if __name__ == "__main__":
+    main()

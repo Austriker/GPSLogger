9365844e47cd7cf5e747bdf75c0730e99481cea0
Hugo Serrat <hu.serrat@gmail.com>
2015-12-02 14:57:30 +0100

    added file logger

--- main.py
+++ main.py
@@ -2,17 +2,28 @@
 from gps3 import gps3
 import time
 import logging
+import os
+import datetime
 
 logger = logging.getLogger(__name__)
+filename = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + "_gps.log"
 
 logger.setLevel(logging.DEBUG)
 
-# create console handler and set level to info
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+handler = logging.FileHandler(os.path.join(output_dir, filename), "w", encoding=None)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+
 
 
 
